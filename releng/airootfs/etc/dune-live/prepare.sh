b6a8817844ec4b8b47d0dc32a0f0c94eac1d8bda
Oromion <caznaranl@uni.pe>
2021-02-17 12:14:36 -0500

    First commit

--- releng/airootfs/etc/dune-live/prepare.sh
+++ releng/airootfs/etc/dune-live/prepare.sh
@@ -19,6 +19,7 @@ ln -sf /usr/share/zoneinfo/America/Lima /etc/localtime
 # Nice help https://bbs.archlinux.org/viewtopic.php?id=243875
 # https://wiki.archlinux.org/index.php/desktop_entries#Hide_desktop_entries
 
+#https://www.linux.com/topic/desktop/cleaning-your-linux-startup-process/
 # Force remove package with pacman https://unix.stackexchange.com/a/10179/292992
 #pacman -Rdd avahi --noconfirm
 echo "NoDisplay=true" >>/usr/share/applications/avahi-discover.desktop
@@ -36,12 +37,25 @@ echo "NoDisplay=true" >>/usr/share/applications/linguist.desktop
 echo "NoDisplay=true" >>/usr/share/applications/qdbusviewer.desktop
 
 pacman -Rdd gnome-disk-utility --noconfirm
-echo "NoDisplay=true" >>/usr/share/applications/nm-connection-editor.desktop
+rm /usr/share/applications/nm-connection-editor.desktop
 #systemctl disable bluetooth.service
 #systemctl mask bluetooth.service
 
 systemctl disable accounts-daemon.service
-#mkdir /home/duneuser/dunerepo
-#repo-add
+
+#https://askubuntu.com/a/294748/791670
 sudo -H -u duneuser bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 sudo -H -u duneuser bash -c "git clone --filter=blob:none --depth=1 https://github.com/hlissner/doom-emacs.git ~/.emacs.d"
+sudo -H -u duneuser bash -c "echo 'export PATH=$HOME/.emacs.d/bin:$PATH' >>~/.zshrc"
+sudo -H -u duneuser bash -c "echo 'xrandr -s 1920x1080' >>~/.zshrc"
+
+sudo -H -u duneuser bash -c "mkdir -p ~/.packages"
+sudo -H -u duneuser bash -c "cd ~/.packages"
+sudo -H -u duneuser bash -c "curl -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/yay-bin.tar.gz"
+sudo -H -u duneuser bash -c "tar -xvf yay-bin.tar.gz"
+sudo -H -u duneuser bash -c "cd yay-bin && makepkg && mv *.zst .. && cd .. && rm -rf yay-bin"
+sudo -H -u duneuser bash -c "sudo repo-add ./custom.db.tar.gz ./*"
+
+sed -i 's/^#\[custom\]/\[custom\]/' /etc/pacman.d/mirrorlist
+sed -i 's/^#SigLevel = Optional TrustAll/SigLevel = Optional TrustAll/' /etc/pacman.d/mirrorlist
+sed -i 's/^#Server = file:///home/custompkgs/Server = file:///home/duneuser/.packages' /etc/pacman.d/mirrorlist
