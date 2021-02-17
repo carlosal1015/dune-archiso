36e1704834ec732015afee21623fa8998c8a9b87
Oromion <caznaranl@uni.pe>
2021-02-17 15:13:05 -0500

    First commit

--- releng/airootfs/etc/dune-live/prepare.sh
+++ releng/airootfs/etc/dune-live/prepare.sh
@@ -44,28 +44,28 @@ echo 'NoDisplay=true' >>/usr/share/applications/qdbusviewer.desktop
 # rm /usr/share/applications/linguist.desktop
 # rm /usr/share/applications/qdbusviewer.desktop
 
-pacman -Rdd gnome-disk-utility --noconfirm
-
 echo 'NoDisplay=true' >>/usr/share/applications/nm-connection-editor.desktop
 # rm /usr/share/applications/nm-connection-editor.desktop
 
+pacman -Rdd gnome-disk-utility --noconfirm
 #systemctl disable bluetooth.service
 #systemctl mask bluetooth.service
 systemctl disable accounts-daemon.service
 
-sudo -H -u duneuser bash -c "echo '[url "ssh://aur@aur.archlinux.org/"]' >>~/.gitconfig"
-sudo -H -u duneuser bash -c "echo '    insteadOf = "aur:"' >>~/.gitconfig"
+sudo -H -u duneuser bash -c 'echo [url \"ssh://aur@aur.archlinux.org/\"] >>~/.gitconfig'
+sudo -H -u duneuser bash -c "echo '    insteadOf = \"aur:\"' >>~/.gitconfig"
 
 #https://askubuntu.com/a/294748/791670
 sudo -H -u duneuser bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 sudo -H -u duneuser bash -c "git clone --filter=blob:none --depth=1 https://github.com/hlissner/doom-emacs.git ~/.emacs.d"
 sudo -H -u duneuser bash -c "echo 'export PATH=\$HOME/.emacs.d/bin:\$PATH' >>~/.zshrc"
 sudo -H -u duneuser bash -c "echo 'xrandr -s 1920x1080' >>~/.zshrc"
-sudo -H -u duneuser bash -c "mkdir -p ~/.packages && cd ~/.packages && curl -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/yay-bin.tar.gz && tar -xvf yay-bin.tar.gz cd yay-bin && makepkg && mv *.zst .. && cd .. && rm -rf yay-bin && sudo repo-add ./custom.db.tar.gz ./*"
+sudo -H -u duneuser bash -c "mkdir -p ~/.packages && cd ~/.packages && curl -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/yay-bin.tar.gz && tar -xvf yay-bin.tar.gz && cd yay-bin && makepkg && mv *.zst .. && cd .. && rm -rf yay-bin && sudo repo-add ./custom.db.tar.gz ./*"
 
 sed -i 's/^#\[custom\]/\[custom\]/' /etc/pacman.conf
 sed -i 's/^#SigLevel = Optional TrustAll/SigLevel = Optional TrustAll/' /etc/pacman.conf
-sed -i 's/custompkgs/duneuser\/.packages/g' /etc/pacman.conf
+sed -i 's/^#Server = file:\/\/\/home\/custompkgs/Server = file:\/\/\/home\/duneuser\/.packages/' /etc/pacman.conf
+#sed -i 's/custompkgs/duneuser\/.packages/g' /etc/pacman.conf
 
 pacman -Sy yay-bin --noconfirm
 #git clone aur:mypackage
