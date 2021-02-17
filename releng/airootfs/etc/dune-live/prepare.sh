e721cc9bedf61b4c30c5e6b32d50f6bb5db7a8c4
Oromion <caznaranl@uni.pe>
2021-02-16 20:10:17 -0500

    First commit

--- releng/airootfs/etc/dune-live/prepare.sh
+++ releng/airootfs/etc/dune-live/prepare.sh
@@ -9,3 +9,11 @@ sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
 
 ln -sf /usr/share/zoneinfo/America/Lima /etc/localtime
+
+xrandr -s 1920x1080 --rate 60
+
+#mkdir /home/duneuser/dunerepo
+#repo-add 
+#git clone --filter=blob:none --depth=1 https://github.com/hlissner/doom-emacs.git /home/duneuser/emacs.d
+
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
