f20303ad97bdde87c3215760b5b5e13ee0ae7d3a
Oromion <caznaranl@uni.pe>
2021-02-17 23:11:13 -0500

    First commit

--- releng/airootfs/etc/dune-live/prepare.sh
+++ releng/airootfs/etc/dune-live/prepare.sh
@@ -18,11 +18,8 @@ ln -sf /usr/share/zoneinfo/America/Lima /etc/localtime
 # xrandr --output VGA1 --mode 1920x1080 --rate 60 --brightness 0.1 --output HDMI1 --mode 1920x1080 --rate 60 --brightness 0.1
 # Nice help https://bbs.archlinux.org/viewtopic.php?id=243875
 # https://wiki.archlinux.org/index.php/desktop_entries#Hide_desktop_entries
-
 # https://www.linux.com/topic/desktop/cleaning-your-linux-startup-process/
-
 # Force remove package with pacman https://unix.stackexchange.com/a/10179/292992
-
 # pacman -Rdd avahi --noconfirm
 # echo 'NoDisplay=true' >>/usr/share/applications/bssh.desktop
 # echo 'NoDisplay=true' >>/usr/share/applications/bvnc.desktop
@@ -62,16 +59,27 @@ sudo -H -u duneuser bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmy
 sudo -H -u duneuser bash -c "git clone --filter=blob:none --depth=1 https://github.com/hlissner/doom-emacs.git ~/.emacs.d"
 sudo -H -u duneuser bash -c "echo 'export PATH=\$HOME/.emacs.d/bin:\$PATH' >>~/.zshrc"
 sudo -H -u duneuser bash -c "echo 'xrandr -s 1920x1080' >>~/.zshrc"
-sudo -H -u duneuser bash -c "mkdir -p ~/.packages && cd ~/.packages && curl -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/yay-bin.tar.gz && tar -xvf yay-bin.tar.gz && rm yay-bin.tar.gz && cd yay-bin && makepkg -s && mv *.zst .. && cd .. && rm -rf yay-bin && sudo repo-add ./custom.db.tar.gz ./*"
+sudo -H -u duneuser bash -c "mkdir -p ~/.packages && cd ~/.packages && curl -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/yay-bin.tar.gz && tar -xvf yay-bin.tar.gz && rm *.tar.gz && cd yay-bin && makepkg && find . -type f ! -name '*.tar.zst' -delete && mv *tar.zst .. && cd .. && rm -rf yay-bin"
+pacman -Sy yay-bin --noconfirm
+
+sudo -H -u duneuser bash -c "cd ~/.packages && yay -G zoom whatsdesk-bin visual-studio-code-bin clion github-desktop-bin jitsi-meet-desktop-bin webex-teams brave-bin"
+# emacs-native-comp-git
+sudo -H -u duneuser bash -c "cd ~/.packages && cd zoom && makepkg && find . -type f ! -name '*.tar.zst' -delete && mv *tar.zst .. && cd .. && rm -rf zoom"
+sudo -H -u duneuser bash -c "cd ~/.packages && cd whatsdesk-bin && makepkg && find . -type f ! -name '*.tar.zst' -delete && mv *tar.zst .. && cd .. && rm -rf whatsdesk-bin"
+# sudo -H -u duneuser bash -c "cd ~/.packages && cd emacs-native-comp-git && makepkg && find . -type f ! -name '*.tar.zst' -delete && mv *tar.zst .. && cd .. && rm -rf emacs-native-comp-git"
+sudo -H -u duneuser bash -c "cd ~/.packages && cd visual-studio-code-bin && makepkg && find . -type f ! -name '*.tar.zst' -delete && mv *tar.zst .. && cd .. && rm -rf visual-studio-code-bin"
+sudo -H -u duneuser bash -c "cd ~/.packages && cd clion && makepkg && find . -type f ! -name '*.tar.zst' -delete && mv *tar.zst .. && cd .. && rm -rf clion"
+sudo -H -u duneuser bash -c "cd ~/.packages && cd github-desktop-bin && makepkg && find . -type f ! -name '*.tar.zst' -delete && mv *tar.zst .. && cd .. && rm -rf github-desktop-bin"
+sudo -H -u duneuser bash -c "cd ~/.packages && cd jitsi-meet-desktop-bin && makepkg && find . -type f ! -name '*.tar.zst' -delete && mv *tar.zst .. && cd .. && rm -rf jitsi-meet-desktop-bin"
+sudo -H -u duneuser bash -c "cd ~/.packages && cd webex-teams && makepkg && find . -type f ! -name '*.tar.zst' -delete && mv *tar.zst .. && cd .. && rm -rf webex-teams"
+sudo -H -u duneuser bash -c "cd ~/.packages && cd brave-bin && makepkg && find . -type f ! -name '*.tar.zst' -delete && mv *tar.zst .. && cd .. && rm -rf brave-bin"
+
+sudo -H -u duneuser bash -c "cd ~/.packages && sudo repo-add ./custom.db.tar.gz ./*"
 
 sed -i 's/^#\[custom\]/\[custom\]/' /etc/pacman.conf
 sed -i 's/^#SigLevel = Optional TrustAll/SigLevel = Optional TrustAll/' /etc/pacman.conf
 sed -i 's/^#Server = file:\/\/\/home\/custompkgs/Server = file:\/\/\/home\/duneuser\/.packages/' /etc/pacman.conf
-#sed -i 's/custompkgs/duneuser\/.packages/g' /etc/pacman.conf
 
-sudo -H -u duneuser bash -c "sudo pacman -Sy yay-bin --noconfirm"
-sudo -H -u duneuser bash -c "yay -Sy zoom --noconfirm"
-sudo -H -u duneuser bash -c "yay -Sy whatsdesk-bin --noconfirm"
-sudo -H -u duneuser bash -c "yay -Sy emacs-native-comp-git --noconfirm"
-#visual-studio-code-bin
-#git clone aur:mypackage
+pacman -S zoom whatsdesk-bin visual-studio-code-bin clion github-desktop-bin jitsi-meet-desktop-bin webex-teams brave-bin --noconfirm
+# emacs-native-comp-git
+# git clone aur:mypackage
